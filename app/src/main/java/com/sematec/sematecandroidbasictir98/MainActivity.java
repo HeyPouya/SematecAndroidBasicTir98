0db9e7d64f106a85a0181a3baf8142c0fd6637f9
Pouya <pouyaheydary@gmail.com>
2019-07-12 10:19:27 +0430

    some changes

--- app/src/main/java/com/sematec/sematecandroidbasictir98/MainActivity.java
+++ app/src/main/java/com/sematec/sematecandroidbasictir98/MainActivity.java
@@ -10,5 +10,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
