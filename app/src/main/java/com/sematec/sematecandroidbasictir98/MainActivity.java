dc5ea299302cec5ee4f8aa684d769f14adce3a1f
Pouya <pouyaheydary@gmail.com>
2019-07-19 13:11:53 +0430

    end of session 3

--- app/src/main/java/com/sematec/sematecandroidbasictir98/MainActivity.java
+++ app/src/main/java/com/sematec/sematecandroidbasictir98/MainActivity.java
@@ -1,8 +1,14 @@
 package com.sematec.sematecandroidbasictir98;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,16 +17,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final TextView txtAge = findViewById(R.id.txtAge);
+        Button btnShowName = findViewById(R.id.btnShowName);
+        final EditText edtAge = findViewById(R.id.edtAge);
+        ImageView imgProfile = findViewById(R.id.imgProfile);
 
 
+        txtAge.setText("Pouya");
 
 
+        btnShowName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String s = edtAge.getText().toString();
+                Log.d("MYTAG", "Result is " + s);
+                txtAge.setText(s);
+            }
+        });
 
+        Log.d("MYTAG", "onCreate");
+    }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("MYTAG", "onStart");
+    }
 
-
-
-
-
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("MYTAG", "onResume");
     }
 }
